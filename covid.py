fba855699690db1ce06fbf8361a431be0a39dc44
SlawekMaciejewski <slawek74.m@interia.pl>
2020-04-15 20:40:07 +0200

    Added writing data to file

--- covid.py
+++ covid.py
@@ -1,16 +1,29 @@
 import re
 import requests
 
+
 def get_number_of_infected():
     page = requests.get('https://www.worldometers.info/coronavirus/country/poland/')
     pattern = r'Poland Coronavirus: (\d,{0,1}\d{0,})'
-
     obj = re.search(pattern, page.text)
-    print(f'Poland Coronavirus: {obj.group(1)}')
+
+    return obj.group(1)
 
 
 # print(page.text)
-# f = open('data.txt', 'w', encoding='utf8')
+# f = open('page.txt', 'w', encoding='utf8')
 # f.write(page.text)
 if __name__ == '__main__':
-    get_number_of_infected()
+    print(f'From the website "Poland Coronavirus" : {get_number_of_infected()} persons')
+
+    with open('data.txt', 'r') as f:
+        SAVED_DATA = f.readline()
+
+    CURRENT_DATA = get_number_of_infected()
+
+    if SAVED_DATA != CURRENT_DATA:
+        with open('data.txt', 'w') as f:
+            f.write(str(CURRENT_DATA))
+        print(f'New data from file, infected are {CURRENT_DATA} persons')
+    else:
+        print('No new infected')
