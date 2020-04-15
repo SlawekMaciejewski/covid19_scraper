969df10bb72c1c2891938bf1e4e92f54b1d398e5
SlawekMaciejewski <slawek74.m@interia.pl>
2020-04-15 20:02:03 +0200

    First solution

--- covid.py
+++ covid.py
@@ -1,5 +1,16 @@
+import re
 import requests
 
+def get_number_of_infected():
+    page = requests.get('https://www.worldometers.info/coronavirus/country/poland/')
+    pattern = r'Poland Coronavirus: (\d,{0,1}\d{0,})'
 
-page = requests.get('https://www.worldometers.info/coronavirus/')
-print(page)
+    obj = re.search(pattern, page.text)
+    print(f'Poland Coronavirus: {obj.group(1)}')
+
+
+# print(page.text)
+# f = open('data.txt', 'w', encoding='utf8')
+# f.write(page.text)
+if __name__ == '__main__':
+    get_number_of_infected()
